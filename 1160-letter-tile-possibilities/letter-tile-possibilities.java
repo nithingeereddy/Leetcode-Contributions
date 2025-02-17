69b72eb2c515b200aefc1ec6f12c32d41040232e
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2025-02-17 17:47:54 -0500

    Time: 16 ms (49.92%) | Memory: 44.4 MB (57.12%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,34 +1,23 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
-        int[] counts = new int[26];
-        int[] fac = new int[tiles.length() + 1];
-        fac[0] = 1;
-        for(int i = 1; i < fac.length; i++){
-            fac[i] = i * fac[i - 1];
+        HashMap<Character,Integer> m=new HashMap<>();
+        for(char c:tiles.toCharArray()){
+            m.put(c,m.getOrDefault(c,0)+1);
         }
-        for(int i = 0; i < tiles.length(); i++){
-            counts[tiles.charAt(i) - 'A']++;
+        return dfs(m);
+    }
+    public int dfs(HashMap<Character,Integer> m)
+    {
+        int count=0;
+        for(char c:m.keySet()){
+            if(m.get(c)>0){
+            count++;
+             m.put(c,m.get(c)-1);
+             count+=dfs(m);
+             m.put(c,m.get(c)+1);
         }
-        int[] lengthCounts = new int[tiles.length() + 1];
-        lengthCounts[0] = 1;
-        for(int i = 0; i < counts.length; i++){
-            if(counts[i] != 0){
-                int[] temp = new int[tiles.length() + 1];
-                for(int j = 0; j < lengthCounts.length && lengthCounts[j] > 0; j++){
-                    for(int k = 1; k <= counts[i]; k++){
-                        int totalLength = j + k;
-                        temp[totalLength] += lengthCounts[j] * fac[totalLength] / (fac[k] * fac[j]);
-                    }
-                }
-                for(int j = 0; j < temp.length; j++){
-                    lengthCounts[j] += temp[j];
-                }
-            }
         }
-        int ans = 0;
-        for(int i = 1; i < lengthCounts.length; i++){
-            ans += lengthCounts[i];
-        }
-        return ans;
+        return count;
+
     }
 }
