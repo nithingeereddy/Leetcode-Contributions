af0fa12f3b5550f9c8b686c34fd915415ad9387a
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2025-07-12 18:49:31 -0400

    Time: 1 ms (99.41%) | Memory: 45.4 MB (32.53%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public int jump(int[] nums) {
-        int coverage = 0, t = 0, lastVisited = 0;
-        if (nums.length == 1) return 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            coverage = Math.max(coverage, i + nums[i]);
-
-            if (i == lastVisited) {
+        int coverage=0,t=0,lastVisited=0;
+        if(nums.length==1) return 0;
+        for(int i=0;i<nums.length-1;i++){
+            coverage=Math.max(coverage,i+nums[i]);
+            if(i==lastVisited){
+                lastVisited=coverage;
                 t++;
-                lastVisited = coverage;
             }
         }
         return t;
