f932dbddbfa43b0cd2ee2e179f3ccf56e8ee80f6
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2025-07-13 13:42:52 -0400

    Time: 8 ms (41.81%) | Memory: 44.7 MB (39.02%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -9,7 +9,7 @@ class Solution {
         boolean M[]=new boolean[n+1];
         M[0]=true;
         for(int i=1;i<=n;i++){
-            for(int j=i-1;j>=Math.max(0,i-maxLen);j--){
+            for(int j=i-1;j>=0;j--){
                 if(M[j] && s1.contains(s.substring(j,i))) M[i]=true;
             }
         }
