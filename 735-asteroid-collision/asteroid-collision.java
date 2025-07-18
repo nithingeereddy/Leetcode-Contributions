0a41593891704107a88776ff52b70e6cd7691a09
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2025-07-17 23:47:54 -0400

    Time: 7 ms (40.78%) | Memory: 45.4 MB (72.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-
-        for (int x : asteroids) {
-            boolean destroyed = false;
-
-            while (!st.isEmpty() && st.peek() > 0 && x < 0) {
-                int top = st.peek();
-
-                if (Math.abs(top) < Math.abs(x)) {
-                    st.pop(); // keep checking further
-                } else if (Math.abs(top) == Math.abs(x)) {
-                    st.pop(); // both explode
-                    destroyed = true;
-                    break;
-                } else {
-                    destroyed = true; // current asteroid is destroyed
+        Stack<Integer> st=new Stack<>();
+       for(int i=0;i<asteroids.length;i++){
+        if(st.isEmpty() || asteroids[i] >0){
+            st.push(asteroids[i]);
+        }
+        else{
+            while(true){
+                if(st.peek()<0){
+                    st.push(asteroids[i]);
                     break;
                 }
-            }
+                else if(st.peek()==-asteroids[i]){
+                    st.pop();
+                    break;
+                }
+                else if(st.peek()>-asteroids[i]){
+                    break;
+                }
+                else{
+                    st.pop();
+                    if(st.isEmpty()){
+                        st.push(asteroids[i]);
+                        break;
+                }
 
-            if (!destroyed) {
-                st.push(x);
             }
         }
-
-        // Convert stack to int[]
-        int[] result = new int[st.size()];
-        for (int i = 0; i < st.size(); i++) {
-            result[i] = st.get(i);
+       }
+       }
+        int r[]=new int[st.size()];
+        for(int i=0;i<r.length;i++){
+            r[i]=st.get(i);
         }
-
-        return result;
+        return r;
     }
 }
