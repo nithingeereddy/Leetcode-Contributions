8ddf2abe4f85df43d53d3dfe133023b4b776e62b
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2025-07-27 10:59:49 -0400

    Time: 2 ms (67.71%) | Memory: 42.3 MB (75.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,7 +1,5 @@
 class Pair{
-    int r=0;
-    int c=0;
-    int tm=0;
+    int r,c,tm;
     Pair(int r,int c,int tm){
         this.r=r;
         this.c=c;
@@ -9,44 +7,45 @@ class Pair{
     }
 }
 class Solution {
+
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int visit[][]=new int[n][m];
-        Queue<Pair> q=new LinkedList<>();
+        int row=grid.length;
+        int col=grid[0].length;
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        Queue<Pair> q=new LinkedList<>();
+        int visit[][]=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]==2){
-                    visit[i][j]=2;
                     q.add(new Pair(i,j,0));
+                    visit[i][j]=2;
                 }
                 else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
-        int t=0,cnt=0,tm=0;
+
         int drow[]={-1,0,+1,0};
         int dcol[]={0,+1,0,-1};
+        int cnt=0,tm=0;
         while(!q.isEmpty()){
-            int r=q.peek().r;
-            int c=q.peek().c;
-            t=q.peek().tm;
-            tm=Math.max(t,tm);
-            q.remove();
-            for(int i=0;i<4;i++){
-                int row=r+drow[i];
-                int col=c+dcol[i];
-                if(row<n && row>=0 && col<m && col>=0 && visit[row][col]==0 && grid[row][col]==1){
-                    visit[row][col]=2;
-                    q.add(new Pair(row,col,t+1));
-                    cnt++;
-                }
+        int r=q.peek().r;
+        int c=q.peek().c;
+        int t=q.peek().tm;
+        tm= Math.max(t,tm);
+       
+        q.poll();
+        for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
+            if(nrow>=0 && nrow<row && ncol>=0 && ncol<col && visit[nrow][ncol]!=2 && grid[nrow][ncol]==1 ){
+                visit[nrow][ncol]=2;
+                q.add(new Pair(nrow,ncol,t+1));
+                cnt++;
             }
         }
-        if(fresh!=cnt) return -1;
-
-        return tm;
     }
+    return fresh==cnt?tm:-1;
+}
 }
