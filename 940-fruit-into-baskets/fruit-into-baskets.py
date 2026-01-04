1a2ad6d8ff9fdaa74148a69ba3e22e55a7f8255c
nithingeereddy <57960064+nithingeereddy@users.noreply.github.com>
2026-01-03 19:16:05 -0500

    Time: 224 ms (29.63%) | Memory: 23.6 MB (69.67%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -4,13 +4,13 @@ class Solution:
         dic={}
         while(r<len(fruits)): 
             dic[fruits[r]]=dic.get(fruits[r],0) + 1
-            r += 1
+           
             while len(dic)>2:
                 dic[fruits[l]]-=1
                 if(dic[fruits[l]]==0):
                     del dic[fruits[l]]
                 l+=1
-            maxi=max(maxi,r-l)
-           
+            maxi=max(maxi,r-l+1)
+            r += 1
 
         return maxi
